7e452436e3096fa2a65aede7a5b8c028e5b4d2a3
slaptat <uzernameic@gmail.com>
2024-07-10 09:35:12 -0500

    yeah ok

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 hi this is just for sharing between computers and junk kthxbye
+
+colors
+ uhhh
+
+
+ panic
+ panik
+ k
+ button./
+
+ clicks
+
+ ihjhjgb
+
+ click counter I mean
+
+ 
--- script.js
+++ script.js
@@ -2,49 +2,51 @@
 
 let showbtn = document.getElementById("showbtn"); 
 
-
-
 // To access the Close button element 
-
 let closebtn = document.getElementById("closebtn"); 
 
-
-
 // To acces the popup element 
-
 let popup = document.querySelector(".popup"); 
-
 let subp = document.getElementById("sub-p"); 
 
-
-
 // To show the popup on click 
-
 showbtn.addEventListener("click", () => { 
-
 	popup.style.display = "block"; 
-
 	showbtn.style.display = "none"; 
-
 	document.body.style.backgroundColor = "#9EA9B1"; 
-
 	subp.style.display = "none"; 
-
 }); 
 
-
-
 // To close the popup on click 
-
 closebtn.addEventListener("click", () => { 
-
 	popup.style.display = "none"; 
-
 	showbtn.style.display = "block"; 
-
 	document.body.style.backgroundColor = "#09dbd450"; 
-
 	subp.style.display = "block"; 
-
 });
 
+var count = 0;
+var displayCount = document.getElementById("count");
+
+var incrementElement = document.getElementById("increment");
+var decrementElement = document.getElementById("decrement");
+var resetElement = document.getElementById("reset");
+
+incrementElement.addEventListener('click',function () {
+    if (count < "1") {
+        count++;
+        displayCount.innerHTML = count;
+    } else {
+        count++;
+        displayCount.innerHTML = count;
+    }   
+});
+decrementElement.addEventListener('click',function () {
+    if (count != "0") {
+        count--;
+        displayCount.innerHTML = count;
+    }   
+});
+resetElement.addEventListener('click',function () {
+    displayCount.innerHTML = "0";   
+});
