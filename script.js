3f2998a9028d725a89ba09f4be8710b869de231e
slaptat <uzernameic@gmail.com>
2024-10-01 00:54:58 -0500

    shrug

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let showbtn = document.getElementById("showbtn");
 // close button 
 let closebtn = document.getElementById("closebtn"); 
 
-
+let test = document.getElementById("test")
 let popup = document.querySelector(".popup"); 
 let subp = document.getElementById("sub-p"); 
 
@@ -13,7 +13,7 @@ let subp = document.getElementById("sub-p");
 showbtn.addEventListener("click", () => { 
 	popup.style.display = "block"; 
 	showbtn.style.display = "none"; 
-	document.body.style.backgroundColor = "#9EA9B1"; 
+	 
 	subp.style.display = "none"; 
 }); 
 
@@ -21,33 +21,60 @@ showbtn.addEventListener("click", () => {
 closebtn.addEventListener("click", () => { 
 	popup.style.display = "none"; 
 	showbtn.style.display = "block"; 
-	document.body.style.backgroundColor = "#09dbd450"; 
+	 
 	subp.style.display = "block"; 
 });
 
 //counter 
-var count = 0;
-var displayCount = document.getElementById("count");
+let count = 0;
+let displayCount = document.getElementById("count");
 
-var incrementElement = document.getElementById("increment");
-var decrementElement = document.getElementById("decrement");
-var resetElement = document.getElementById("reset");
 
-incrementElement.addEventListener('click',function () {
-    if (count < "1") {
-        count++;
-        displayCount.innerHTML = count;
-    } else {
-        count++;
-        displayCount.innerHTML = count;
-    }   
+
+
+
+showbtn.addEventListener('click',function () {
+    count++;
+    displayCount.innerHTML = count;
+    if (count == 3) {
+        window.confirm("Dang good button there.");
+    }
+    if (count == 4) {
+        window.confirm("See! I learned something.");
+    }
+    if (count == 5) {
+        window.confirm("Never trust Greg.");
+    }
+    if (count == 9) {
+        window.confirm("Oh, youre still clicking...ok..cool.");
+    }
+    if (count == 10) {
+        window.confirm("No need to keep clicking...nothing else to click to.");
+    }
+    if (count == 12) {
+        window.confirm("Unless you're bored, then I guess keep clicking. No harm no fowl.");
+    }
+    if (count == 14) {
+        window.confirm("Ok, I guess im going to head home.");
+    }
+    if (count == 20) {
+        window.alert("20! go head. look at you. A real Champion. OK for real, Im out.");
+    }
+    if (count == 73) {
+        window.confirm("Sheesh, still going huh. No plans tonight?");
+    }
+    if (count == 107) {
+        window.confirm("Alright, go play a real game or something, this thing isnt even keeping high score. Go. Bye. No more clicks.");
+    }
+    if (count == 666) {
+        window.alert("Gregs literally the devil");
+    }
+    if (count == 1000) {
+        window.confirm("Incredible. Go touch grass.");
+    }
 });
-decrementElement.addEventListener('click',function () {
-    if (count != "0") {
-        count--;
-        displayCount.innerHTML = count;
-    }   
+
+count.addEventListener('click', function () {
+
 });
-resetElement.addEventListener('click',function () {
-    displayCount.innerHTML = "0";   
-});
+       
